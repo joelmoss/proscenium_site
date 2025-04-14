a83ad12262046bccc72691a25bb55625d078d72c
Joel Moss <joel@developwithstyle.com>
2025-04-14 22:44:45 +0100

    chore: update proscenium cache query string to use heroku release version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.proscenium.cache_query_string = ENV.fetch('HEROKU_RELEASE_VERSION', nil)
+
   config.hosts << 'proscenium.rocks'
   config.hosts << 'registry.proscenium.rocks'
 
