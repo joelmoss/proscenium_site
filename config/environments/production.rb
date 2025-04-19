ab06a2c7b2573da2380f4c5eb640361fa399f6af
Joel Moss <joel@developwithstyle.com>
2025-04-19 13:07:22 +0100

    feat: Set Sentry release and use KAMAL_VERSION for Proscenium cache_query_string

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.proscenium.cache_query_string = ENV.fetch('KAMAL_VERSION')
+
   # config.hosts << 'proscenium.rocks'
   # config.hosts << 'registry.proscenium.rocks'
 
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,7 @@ if defined?(Sentry)
   Sentry.init do |config|
     config.dsn = ENV['SENTRY_DSN']
     config.environment = Rails.env
+    config.release = ENV.fetch('KAMAL_VERSION', nil)
 
     config.traces_sample_rate = 1.0
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
